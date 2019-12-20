ce55a2422b24b79779501c86e8b54dfac6d71bcb
Parul Mishra Dubedy <parul.mishra78@gmail.com>
2019-12-20 08:12:56 -0800

    Removing the basic sonar build

--- .pipeline/lib/sonar.js
+++ .pipeline/lib/sonar.js
@@ -10,14 +10,14 @@ module.exports = (settings)=>{
   let objects = []
   const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
 
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/bcdevexchange-sonar-build.yaml`, {
-    'param':{
-      'NAME': phases[phase].name,
-      'SOURCE_REPOSITORY_URL': oc.git.http_url,
-      'SOURCE_REPOSITORY_REF': oc.git.ref,
-      'SOURCE_CONTEXT_DIR': 'dotnet-sonar'
-    }
-}))
+//   objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/bcdevexchange-sonar-build.yaml`, {
+//     'param':{
+//       'NAME': phases[phase].name,
+//       'SOURCE_REPOSITORY_URL': oc.git.http_url,
+//       'SOURCE_REPOSITORY_REF': oc.git.ref,
+//       'SOURCE_CONTEXT_DIR': 'dotnet-sonar'
+//     }
+// }))
 
   objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/bcdevexchange-sonar.yaml`, {
     'param':{
