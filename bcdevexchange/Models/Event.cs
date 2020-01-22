58e9991e9d1af842a23aaadfc524fab4f01f9278
parulmishra <parul.mishra78@gmail.com>
2020-01-22 15:21:59 -0800

    Made a small change to deploy again

--- bcdevexchange/Models/Event.cs
+++ bcdevexchange/Models/Event.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace bcdevexchange.Models
 {
