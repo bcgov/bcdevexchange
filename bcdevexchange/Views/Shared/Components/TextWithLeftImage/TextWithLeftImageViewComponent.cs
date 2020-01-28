ef39e029c7988225d963c9c93310ca8fd2a30335
jsharman <jon.w.sharman@gmail.com>
2020-01-28 09:36:52 -0800

    The sample view componnent is done

---
+++ bcdevexchange/Views/Shared/Components/TextWithLeftImage/TextWithLeftImageViewComponent.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bcdevexchange.Components.TextWithLeftImage 
+{
+  public class TextWithLeftImageViewComponent : ViewComponent
+  {
+    public TextWithLeftImageViewComponent()
+    {
+
+    }
+
+    public IViewComponentResult Invoke(string textWithLeftImageControlType) 
+    {
+      return View("Default", textWithLeftImageControlType);
+    }
+  }
+
+}
