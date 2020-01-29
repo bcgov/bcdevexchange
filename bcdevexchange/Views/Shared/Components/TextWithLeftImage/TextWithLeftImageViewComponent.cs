c028cdb5b8ad5e39dcc03d68cc199135fef6c5a8
jsharman <jon.w.sharman@gmail.com>
2020-01-29 11:11:53 -0800

    The view component is working properly

--- bcdevexchange/Views/Shared/Components/TextWithLeftImage/TextWithLeftImageViewComponent.cs
+++ bcdevexchange/Views/Shared/Components/TextWithLeftImage/TextWithLeftImageViewComponent.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using bcdevexchange.Models;
+
+
 
 namespace bcdevexchange.Components.TextWithLeftImage 
 {
+  public class TextWithLeftImageModel 
+  {
+    public bool TextOnLeft {get; set;}
+    public string Title {get; set;}
+    public string SubTitle {get; set;}
+    public string ImageFilePath {get; set;}
+    public string ButtonUrl {get; set;}
+
+  }
+
   public class TextWithLeftImageViewComponent : ViewComponent
   {
     public TextWithLeftImageViewComponent()
@@ -18,9 +31,20 @@ namespace bcdevexchange.Components.TextWithLeftImage
     //   return View("Default", textWithLeftImageControlType);
     // }
 
-    public IViewComponentResult Invoke(string title, string text) 
+    public IViewComponentResult Invoke(bool textOnLeft, string title, string subTitle, string buttonUrl, string imageFilePath) 
     {
-      return View("Default", textWithLeftImageControlType);
+
+      
+      TextWithLeftImageModel textWithLeftImageModel = new TextWithLeftImageModel
+      {
+        TextOnLeft = textOnLeft,
+        Title = title, 
+        SubTitle = subTitle, 
+        ButtonUrl = buttonUrl, 
+        ImageFilePath = imageFilePath
+      };
+
+      return View("Default", textWithLeftImageModel);
     }
   }
 
