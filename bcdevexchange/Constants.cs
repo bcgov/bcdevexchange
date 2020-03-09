eeb3c876d9da89b31afa628ad439d5c477ad912c
Parul Mishra Dubedy <parul.mishra78@gmail.com>
2020-03-09 08:06:34 -0700

    testing deployment again

--- bcdevexchange/Constants.cs
+++ bcdevexchange/Constants.cs
@@ -3,10 +3,15 @@
     public static class Constants
     {
         public const string CourseFormatId = "9";
+
         public const string EnvPath = "../../../.env";
+
         public const string LiveEventStatus = "live";
+
         public const string CoursesKey = "coursesKey";
+
         public const string EventsKey = "eventsKey";
+        
         public const int ExpirationTimeInMinutes = 60;
     }
 }
